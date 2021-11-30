e29745582150edf511438a27648a1bfdfeb0dde3
thidayu <tanthidayu_0000@hotmail.com>
2021-11-30 16:50:50 +0700

    "lab7.3 finished"

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,17 @@
-#include<iostream>
-
-using namespace std;
-
-
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+int adiff(int x,int y){
+  int t;
+  while(x > 360){
+    x -= 360;}
+  while(y > 360){
+    y -= 360;}
+  if(x > y){
+    t = x - y;
+  }else{
+    t = y - x;}
+  while(t >= 360){
+    t -= 360;}
+  if(t>180){
+    t = 360 - t;
+  }
+  return t ;
 }
